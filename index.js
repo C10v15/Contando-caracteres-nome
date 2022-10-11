6f65f4fd4403de2adaa6faaa98194cd9d2a041de
Clovis Santiago <sant_sivolc@hotmail.com>
2022-10-11 08:29:59 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,12 @@
+alert('Olá seja bem vindo(a)!')
+const retornoUsuario = confirm('Vamos interagir?')
+
+if(retornoUsuario){
+    let nome = prompt("Escreva seu nome") 
+    alert('A quantidade de caracteres no seu nome é ' + nome.length) 
+}else{
+     alert("Que pena! Você parecia ser uma pessoa mais legal :(")
+}
+
+
+
